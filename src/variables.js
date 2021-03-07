8eba09518856bf025203c40d23dbc802852385a1
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-03-07 18:16:18 +0100

    Initial commit

--- src/variables.js
+++ src/variables.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "",
-  description: "",
-  heading1: "",
-  heading2: "",
-  githubLink: "",
-  npmLink: "",
-  subPath: "",
-  linkHome: false,
+  title: "de-noun-parser",
+  description: "Parse Wikitext's de-noun template to extract the gender and plural, diminutive, genetive and gendered forms of German nouns.",
+  heading1: "de-noun-parser",
+  heading2: "Parse Wikitext's de-noun template",
+  githubLink: "https://github.com/maroun-baydoun/de-noun-parser",
+  npmLink: "https://www.npmjs.com/package/de-noun-parser",
+  subPath: "de-noun-parser/",
+  linkHome: true,
 };
